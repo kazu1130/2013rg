da3e90c44a361bec6d3b0700ee6837bb638d123e
kucats <kucats@kuc.jp>
2013-05-23 17:50:33 +0900

    Update func_loginMethod.php

--- func_loginMethod.php
+++ func_loginMethod.php
@@ -6,24 +6,24 @@ function checkLoginRequest($user_id,$passwd){
   $seed=""61737b06cff3d7f999c297d96f2b20007521eee1";
   
   $dbobj = DBConnection::getInstance();
-  $user_array=$dbobj->fetchALL("SELECT * from userdata WHERE uname = ?",array($user_id));
-  $passwd_hash=sha1($seed.$passwd);
-  if($user_array['passwd']!=$passwd_hash){
+  $user_array=$dbobj->fetchALL("SELECT * from userdata WHERE login_id = ?",array($login_id));
+  $pass_hash=sha1($seed.$pass);
+  if($user_array['passwd']!=$pass_hash){
     perror('6000','login_error');
     return false;
   }else{
     //MAKE RANDOMIZE ALPHABET//
-    $secretToken=sha1($seed.$user_array['uname'].time());
+    $secretToken=sha1($seed.$user_array['login_id'].time());
     //UPDATE TO DATABASE//
-    $dbobj->fetchALL("UPDATE usersessions SET ? = '?' WHERE uname= ?",array($user_id,$secretToken,$user_id));
+    $dbobj->fetchALL("UPDATE usersessions SET ? = '?' WHERE login_id= ?",array($login_id,$secretToken,$user_id));
         
     //SET ENV//
     //return true;
     return $secretToken;
   }
-function getSecretToken($user_id){
+function getSecretToken($login_id){
   $dbobj = DBConnection::getInstance();
-  $user_array=$dbobj->fetchALL("SELECT * from usersessions WHERE uname = ?",array($user_id));
+  $user_array=$dbobj->fetchALL("SELECT * from usersessions WHERE login_id = ?",array($login_id));
   $secretToken=$user_array['token'];
   return $secretToken;
 }
