6ec7073be9b983f5ef35346f5655b5f8241491a7
kazu1130 <kazu1130@hotmail.com>
2013-05-30 15:42:55 +0900

    Update consts.php

--- consts.php
+++ consts.php
@@ -3,5 +3,7 @@ class CONSTS {
   const DB_USER = "t11767kh"; //各自のユーザID
   const DB_PASS = "scaBXvtW"; //各自のdbパス
   const DB_SN = "pgsql:dbname=t11767kh;host=webdb.sfc.keio.ac.jp";
+  
+  const HASH_SEED = "61737b06cff3d7f999c297d96f2b20007521eee1";  
 }
 ?>
