c1ccc979912ae84b1aaf7c5e611bc146145bbbd7
kucats <kucats@kuc.jp>
2013-05-30 16:30:01 +0900

    Create postcomments.php

---
+++ postcomments.php
@@ -0,0 +1,11 @@
+<?php
+require_once('./class/consts.php');
+require_once('./class/dbconnect.php');
+require_once('./class/user.php');
+
+$user = User::getInstance($_GET["session_id"]);
+$dbh = DBConnection::getInstance();
+
+
+
+?>
