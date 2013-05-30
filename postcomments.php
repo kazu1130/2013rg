2630e426b424bde56c3980450d0eab8bfa78bfe0
kazu1130 <kazu1130@sfc.wide.ad.jp>
2013-05-30 17:38:09 +0900

    change postcomment,login

--- postcomments.php
+++ postcomments.php
@@ -8,20 +8,16 @@ $dbh = DBConnection::getInstance();
 
 //check posted data
 $comment = htmlspecialchars($_POST["comment"]);
-$session_id = htmlspecialchars($_POST["session"]);
 $store_id = htmlspecialchars($_POST["store_id"]);
 
-if(!$comment || !$session_id || !$store_id){
+if(!$comment || !$store_id){
  perror('2000','lack_of_data','必要な情報が不足しています');
  exit;
 }
-if($uid=checkSecretToken($_session_id)===false){
-  perror('7000','invalid_token','セッション確立エラー');
-  exit;
-}
-$update_array = array($uid,$store_id,$comment,time());
+
+$update_array = array($user->id,$store_id,$comment,time());
 $result_update = $dbobj->fetchALL("INSERT INTO comments (user_id,store_id,comment,time) VALUES (?,?,?,?,?)",array($update_array));
 
-print json_encode(array("status"=>success,$ret_sotres));
+print json_encode(array("status"=>success));
 
 ?>
