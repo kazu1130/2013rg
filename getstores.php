3bddfdc3520d194f2ec2501916d2a2d02e677360
kazu1130 <kazu1130@sfc.wide.ad.jp>
2013-05-30 16:23:17 +0900

    make getstors

--- getstores.php
+++ getstores.php
@@ -4,7 +4,18 @@ require_once('./class/dbconnect.php');
 require_once('./class/user.php');
 
 
+$mparlat = 111263.28336;
+$mparlan = 91159.16112;
+
 $user = User::getInstance($_GET["session_id"]);
+
+$dbh = DBConnection::getInstance();
+
+$stores = $dbh->fetchAll("SELECT * FROM stores WHERE lat > ? AND lon = ?",array());
+
+
+$_GET["lat"]
+$_GET["lon"]
 
 
 
